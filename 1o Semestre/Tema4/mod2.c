1701759a8a0c8e058fea8bf76db6c102459451b0
Pedro Verner <pedrow21br@gmail.com>
2022-04-12 19:58:44 -0300

    Teste de exercico

---
+++ 1o Semestre/Tema4/mod2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Exercicios
+
+void main()
+{
+
+    char f, a;
+
+    f = "f";
+
+    a = "F";
+
+    if (a == f)
+    {
+        printf("Positive");
+    }
+    else
+    {
+        printf("Negative");
+    }
+}
