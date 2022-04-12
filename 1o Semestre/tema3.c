3de3ebb2a564db821581bc9c551e5bd26bcdd278
Pedro Verner <pedrow21br@gmail.com>
2022-04-12 18:01:30 -0300

    Mostrando informações no printf com variaveis

--- 1o Semestre/tema3.c
+++ 1o Semestre/tema3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 void main () {
+
+    int day = 12;
+
     printf("Hello World!!!\n");
-    printf("Is such a beautiful day, don't you think?");
+    printf("Is such a beautiful day, don't you think?\n");
+    printf("Ah, and just to say, today is %d", day);
 }
