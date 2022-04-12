4b524c3f32bb26719f31610b4504aa57d94b17db
Pedro Verner <pedrow21br@gmail.com>
2022-04-12 19:12:59 -0300

    Fim do tema 3

---
+++ 1o Semestre/Tema3/mod3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+
+    int age;
+    char sex;
+
+    puts("Welcome, what's your age?\n");
+    scanf("%i", &age);
+
+    printf("Congratulations!! You have %i years\n", age);
+}
