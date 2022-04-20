4b243a402db1a8ccdd02b470148f5e3b0aeb5444
Pedro Verner <pedrow21br@gmail.com>
2022-04-20 17:25:48 -0300

    Manipulando o DOM com jQuery

--- 3o Semestre/Programação Cliente Servidor/Tema01/Modulo02/index.js
+++ 3o Semestre/Programação Cliente Servidor/Tema01/Modulo02/index.js
@@ -8,3 +8,8 @@ $(".list-aboutJquery").append("<li>Item inserido pelo jQuery</li>");
 $(".title").after("<p>Olá! Item inserido pelo jQuery</p>");
 
 $(".list-item").eq(3).remove();
+
+$(".title").html("Trocando o Titulo pelo jQuery =)");
+
+$("li").first().html($("li").first().html() + " Oi, fui inserido por script aqui O.o"); // Seleciona o conteudo do primeiro elemento li, e faz com que seu conteudo seja ele mesmo mais o que foi concatenado
+
