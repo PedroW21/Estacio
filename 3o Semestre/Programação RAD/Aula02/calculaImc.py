9b99c7282855e450aae81bbec16de64111575054
Pedro Verner <pedrow21br@gmail.com>
2022-04-19 21:12:41 -0300

    Calculando imc

---
+++ 3o Semestre/Programação RAD/Aula02/calculaImc.py
@@ -0,0 +1,13 @@
+# Programa em python para calcular imc
+
+altura = float(input("Por favor informe sua altura: "))
+peso = float(input("Por favor informe seu peso: "))
+
+imc = peso / (altura * altura)
+
+print("O seu IMC é: ", imc) 
+
+
+
+
+
