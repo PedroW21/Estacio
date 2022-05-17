0794ffe7458949324d98f36cff9e7a8e72c8a1a3
Pedro Verner <pedrow21br@gmail.com>
2022-05-17 07:09:11 -0300

    Fim da aula 2

---
+++ 3o Semestre/Programação RAD/Aula02/mediaBimestre.py
@@ -0,0 +1,14 @@
+#Faça um programa que peça as quatro notas bimestrais e mostre a média.
+
+print("------------ MÉDIA BIMESTRAL ------------")
+print("")
+
+nota1 = float(input("Digite sua primeira nota: "))
+nota2 = float(input("Digite sua primeira nota: "))
+nota3 = float(input("Digite sua primeira nota: "))
+nota4 = float(input("Digite sua primeira nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print("")
+print("Sua média bimestral foi: ", media)
