bbb2eb7bb199b34d8b09dbfdd368412fb8bf6056
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-05-31 09:42:19 -0300

    Changing directory name

---
+++ 3o-Semestre/Programacao RAD/Aula08/ManipulandoArquivos.py
@@ -0,0 +1,4 @@
+ajaxEx = open("C:\\Users\\Pedro Verner\\Desktop\\Estacio\\3o-Semestre\\Programaçao RAD\\Aula08\\AJAX-Example.txt", "w")
+
+for line in ajaxEx:
+    print(line)
--- 3o-Semestre/Programaçao RAD/Aula08/ManipulandoArquivos.py
+++
@@ -1,4 +0,0 @@
-ajaxEx = open("C:\\Users\\Pedro Verner\\Desktop\\Estacio\\3o-Semestre\\Programação RAD\\Aula08\\AJAX-Example.txt", "w")
-
-for line in ajaxEx:
-    print(line)
